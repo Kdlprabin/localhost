5fd47ce5be5df41c680e7932eda77967cb0b9599
prabin <kdlprabin444@gmail.com>
2022-07-29 11:05:37 +0545

    form update

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 
+$name = $_POST['name'];
+$password=$_POST['password'];
+
+
+$sql= "INSERT INTO form(name,password) VALUES('$name','$password');";
+
 $conn = new mysqli('localhost','root','','form','3307');
+
+$result = mysqli_query($conn,$sql);
 
 if($conn->connect_error){
     die("Connection Failed");
 }
 else{
-    echo "Connection Sucessful";
+    echo "Connection Sucessful\n";
+    if($result){
+        echo "\nSignUp sucessful";
+    }else{
+        echo "SignUp Failed";
+    }
 }
 ?>
