033fb220b8a13458b890aaf896f94d376e053aa3
prabin <kdlprabin444@gmail.com>
2022-07-29 13:01:59 +0545

    connection to the database made with hash password

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
-
-$name = $_POST['name'];
-$password=$_POST['password'];
-
-
-$sql= "INSERT INTO form(name,password) VALUES('$name','$password');";
+echo "connection success";
+$email = $_POST['email'];
+$password=md5($_POST['password']);
+$address = $_POST['address'];
+$address2 = $_POST['address2'];
+$city = $_POST['city'];
+$state = $_POST['state'];
 
 $conn = new mysqli('localhost','root','','form','3307');
+
+$sql= "INSERT INTO form(email,password,address,address2,state,city) VALUES('$email','$password','$address','$address2','$state','$city');";
 
 $result = mysqli_query($conn,$sql);
 
